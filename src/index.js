7699683483981bc774f5eb54de355e9785506514
Erika Espejo <72981530+ErikaEspejo@users.noreply.github.com>
2021-06-20 10:57:52 -0500

    JSX components and props

--- src/index.js
+++ src/index.js
@@ -3,23 +3,43 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 
-const song1 = "Smells Like Teen Spirit";
-const song2 = "Come As You Are";
+/*
+const item = ({ title }) => {
+  //se traen los props de los elementos que se renderizan y se usan para renderizar algo mas
 
-const item = (title) => {
   return <p>{title}</p>;
 };
 
 const element = (
   <>
-    {item(song1)}
-    {item(song2)}
+    {item({
+      title: "Smells Like Teen Spirit"
+    })}
+    {item({
+      title: "Come As You Are"
+    })}
+  </>
+);
+*/
+
+//Esta es una manera correcta de hacerlo y de mejor entendimiento
+
+const Item = ({ title }) => {
+  //trae de los props de Item el title
+  return <p>{title}</p>;
+};
+
+const element = (
+  <>
+    <Item title="Smells Like Teen Spirit" />
+    <Item title="Come As You Are" />
   </>
 );
 
 /*
-  React Trabaja con componentes, lo anterior es un ejemplo muy basico
-  de como funcionaria uno por debajo.
+  JSX puede diferenciar una etiqueta HTML de un componente por su inicial,
+  si esta es en mayuscula se trata de un componente, en caso contrario es
+  una etiqueta HTML.
 */
 
 ReactDOM.render(element, root);
