55dc8863d6ceb2111bb16616a0259fa349305db3
Erika Espejo <72981530+ErikaEspejo@users.noreply.github.com>
2021-06-20 09:38:49 -0500

    create elements without jsx

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 
-const element = React.createElement("div", {
-  className: "container",
-  children: "hello world"
-});
+const element = React.createElement(
+  "div",
+  {
+    className: "container"
+  },
+  React.createElement("p", null, "Hola mundo")
+);
 
 ReactDOM.render(element, root);
