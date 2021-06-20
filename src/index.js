f23dccf9ea5c721f2d604cb3b31cf43b4f5a95ca
Erika Espejo <72981530+ErikaEspejo@users.noreply.github.com>
2021-06-20 11:43:01 -0500

    reference functions in events

--- src/index.js
+++ src/index.js
@@ -3,33 +3,31 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 
-const Item = ({ title, subtitle, ...props }) => {
-  //traigo los demas props y los uso en el span, como solo es style pues asigna los estilos al span
+const Item = ({ title, subtitle }) => {
   return (
-    <span {...props}>
+    <span>
       {title} - {subtitle}
     </span>
   );
+};
+
+const handleEvent = (event) => {
+  console.log(event);
 };
 
 const element = (
   <>
     <Item title="Smells Like Teen Spirit" subtitle={<strong>Nirvana</strong>} />
     <br />
-    <Item
-      title="Come As You Are"
-      subtitle="Nirvana"
-      style={{
-        borderColor: "red",
-        borderWidth: 2,
-        borderStyle: "dotted"
-      }}
-    />
+    <Item title="Come As You Are" subtitle="Nirvana" />
+    <br />
+    <button onClick={handleEvent}>Shuffle</button>
   </>
 );
 
 /*
-  Los props tambien pueden recibir JSX. 
+  No se ponen parentesis para el handleEvent(), porque se requiere referenciar 
+  la funcion y no ejecutarla cada vez que se pinte, sino cuando se de click.
 */
 
 ReactDOM.render(element, root);
