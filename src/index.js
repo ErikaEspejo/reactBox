9b101d2a7379342462a85b62c7005105e383b51e
Erika Espejo <72981530+ErikaEspejo@users.noreply.github.com>
2021-06-20 11:16:51 -0500

    props and jsx

--- src/index.js
+++ src/index.js
@@ -3,48 +3,24 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 
-/*
-const item = ({ title }) => {
-  //se traen los props de los elementos que se renderizan y se usan para renderizar algo mas
-
-  return <p>{title}</p>;
-};
-
-const element = (
-  <>
-    {item({
-      title: "Smells Like Teen Spirit"
-    })}
-    {item({
-      title: "Come As You Are"
-    })}
-  </>
-);
-*/
-
-//Esta es una manera correcta de hacerlo y de mejor entendimiento
-
-const Item = ({ title, children }) => {
-  //trae los children de Item, es decir lo que esta dentro de las etiquetas y renderiza lo que aparece en el componente
-  //trae de los props de Item el title
+const Item = ({ title, subtitle }) => {
   return (
-    <p>
-      {title} - {children}
-    </p>
+    <span>
+      {title} - {subtitle}
+    </span>
   );
 };
 
 const element = (
   <>
-    <Item title="Smells Like Teen Spirit">Nirvana</Item>
-    <Item title="Come As You Are" />
+    <Item title="Smells Like Teen Spirit" subtitle={<strong>Nirvana</strong>} />
+    <br />
+    <Item title="Come As You Are" subtitle="Nirvana" />
   </>
 );
 
 /*
-  JSX puede diferenciar una etiqueta HTML de un componente por su inicial,
-  si esta es en mayuscula se trata de un componente, en caso contrario es
-  una etiqueta HTML.
+  Los props tambien pueden recibir JSX. 
 */
 
 ReactDOM.render(element, root);
